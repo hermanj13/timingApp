a90a97b1d476b2a3816455f1ff042811b3ae9d09
hermanj13 <hermanj13@me.com>
2017-07-24 17:54:58 -0400

    figured out data structure

---
+++ data/week-01/data.js
@@ -0,0 +1,10 @@
+var day01 = require('./day-01/data.js');
+var day02 = require('./day-02/data.js');
+var day02 = require('./day-02/data.js');
+var day02 = require('./day-02/data.js');
+var day02 = require('./day-02/data.js');
+
+console.log(day01)
+exports.week1 = {
+  day01: day01
+}
